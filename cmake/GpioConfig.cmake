8c9fdcc80b79d3d2fd6e83958e5557cbecb6b27e
cpasjuste <cpasjuste@gmail.com>
2024-11-13 18:19:37 +0100

    code: minor cleanup

--- cmake/GpioConfig.cmake
+++ cmake/GpioConfig.cmake
@@ -7,8 +7,8 @@ function(add_gpio_definition flags gpio_name gpio_pin)
     if (DEFINED ${gpio_name})
         # Use the command line value instead
         list(APPEND ${flags} "-D${gpio_name}=${${gpio_name}}")
-        message(STATUS "Using command line GPIO: ${gpio_name} => ${${gpio_name}}")
-    else()
+        message(STATUS "${gpio_name} => ${${gpio_name}}")
+    else ()
         # No command line value, use the provided pin
         if (NOT DEFINED gpio_pin)  # Changed from "if (NOT gpio_pin)"
             message(FATAL_ERROR "GPIO PIN not specified")
@@ -17,8 +17,8 @@ function(add_gpio_definition flags gpio_name gpio_pin)
         # Set the cache variable for future cmake runs
         set(${gpio_name} "${gpio_pin}" CACHE STRING "${gpio_name}")
         list(APPEND ${flags} "-D${gpio_name}=${gpio_pin}")
-        message(STATUS "Added GPIO: ${gpio_name} => ${gpio_pin}")
-    endif()
+        message(STATUS "${gpio_name} => ${gpio_pin}")
+    endif ()
 
     # Propagate changes to parent scope
     set(${flags} "${${flags}}" PARENT_SCOPE)
