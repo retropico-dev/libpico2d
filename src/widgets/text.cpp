06a40a4c514cc3b8bd79e5519ce94be082dd7ec6
cpasjuste <cpasjuste@gmail.com>
2025-01-03 17:25:16 +0100

    text: fix clip area

--- src/widgets/text.cpp
+++ src/widgets/text.cpp
@@ -11,11 +11,8 @@ Text::Text(const int16_t x, const int16_t y,
            const std::string &str, const Display::Color color) : Widget() {
     // default stuff
     Platform::instance()->getDisplay()->setTextWrap(false);
-    Platform::instance()->getDisplay()->getTextBounds(
-        str.c_str(), 0, 0, &m_text_bounds.x, &m_text_bounds.y,
-        reinterpret_cast<uint16_t *>(&m_text_bounds.w), reinterpret_cast<uint16_t *>(&m_text_bounds.h));
-    m_max_size = {m_text_bounds.w, m_text_bounds.h};
-    Widget::setPositionAndSize(x, y, m_max_size.x, m_max_size.y);
+
+    Widget::setPosition(x, y);
     m_color = color;
     setString(str);
 }
@@ -24,6 +21,7 @@ Text::Text(const int16_t x, const int16_t y, const int16_t w, const int16_t h,
            const std::string &str, const Display::Color color) : Widget() {
     // default stuff
     Platform::instance()->getDisplay()->setTextWrap(false);
+
     Widget::setPositionAndSize(x, y, w, h);
     m_max_size = {w, h};
     m_color = color;
@@ -34,6 +32,7 @@ Text::Text(const Utility::Vec2i &pos, const Utility::Vec2i &size,
            const std::string &str, Display::Color color) : Widget() {
     // default stuff
     Platform::instance()->getDisplay()->setTextWrap(false);
+
     Widget::setPositionAndSize(pos.x, pos.y, size.x, size.y);
     m_max_size = size;
     m_color = color;
@@ -46,13 +45,13 @@ std::string Text::getString() const {
 
 void Text::setString(const std::string &str) {
     m_text = str;
+
+    // update text bounds if no "max size" was specified
     Platform::instance()->getDisplay()->getTextBounds(
         m_text.c_str(), 0, 0, &m_text_bounds.x, &m_text_bounds.y,
         reinterpret_cast<uint16_t *>(&m_text_bounds.w), reinterpret_cast<uint16_t *>(&m_text_bounds.h));
-    if (m_max_size.x != 0 || m_max_size.y != 0) {
+    if (m_max_size.x == 0 && m_max_size.y == 0) {
         setSize(m_text_bounds.w, m_text_bounds.h);
-    } else {
-        setSize(m_max_size);
     }
 }
 
@@ -88,7 +87,7 @@ void Text::onUpdate(const Time delta) {
 void Text::onDraw(const Utility::Vec4i &bounds) {
     // now draw the text
     Platform::instance()->getDisplay()->setTextColor(m_color);
-    //Platform::instance()->getDisplay()->setClipArea({bounds.x, bounds.y, m_max_size.x, m_max_size.y});
+    Platform::instance()->getDisplay()->setClipArea({bounds.x, bounds.y, bounds.w, bounds.h});
     Platform::instance()->getDisplay()->drawText(static_cast<int16_t>(bounds.x - m_tween_pos_x), bounds.y, m_text);
     Platform::instance()->getDisplay()->setClipArea(
         {
